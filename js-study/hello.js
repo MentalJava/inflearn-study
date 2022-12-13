0e94be720e1ca5072712886e71d39a99f3d6acbc
MentalJava <107596556+MentalJava@users.noreply.github.com>
2022-12-13 13:27:03 +0900

    콜백, 익명함수

--- js-study/hello.js
+++ js-study/hello.js
@@ -9,3 +9,36 @@ function returnProducts(products) {
   }
   return newProducts;
 }
+
+function sayHello() {
+  console.log("나는 유준입니다.");
+}
+
+function getHuman(callback) {
+  callback();
+}
+
+getHuman(sayHello);
+
+setTimeout(function () {
+  console.log("3초 뒤에 실행됩니다.");
+}, 3000);
+
+setInterval(function () {
+  console.log("1초마다 출력됩니다.");
+}, 1000);
+
+function callbackFunc(name) {
+  console.log(name);
+}
+
+function sayHello(callback) {
+  var text = "hello";
+  callback(text);
+}
+
+sayHello(callbackFunc);
+
+sayHello(function (value) {
+  console.log(value);
+});
