2b062f4ad9eb854fc511046ddaf8d13f7254db3b
MentalJava <107596556+MentalJava@users.noreply.github.com>
2022-12-20 14:36:41 +0900

    Data type

--- js-study/HI.js
+++ js-study/HI.js
@@ -18,3 +18,37 @@ secondword = "def";
 /* var 재할당, 재선언 O */
 var uniqueWord = "unique";
 var uniqueWord = "ununiqueWord";
+
+/*  camelCase snake_case PascalCase */
+
+/* String, Number */
+console.log("Hello " + "world");
+
+console.log(1 + 2);
+
+console.log(3 - 1);
+
+console.log(10 / 2);
+
+console.log("a" + 1); /* 문자 + 숫자는 문자열로 반환 */
+
+console.log("1" + 1); /* 문자열 11 반환 */
+
+console.log(1 + ""); /* 문자열로 변환 */
+
+console.log("a" * 1); /* NaN (Not a Number) */
+
+console.log("2" * 2); /* 숫자로 변환 후 4로 반환 */
+
+/* Array, Object */
+let ranking = [
+  "jason",
+  "Alice",
+  "Chris",
+  "Jane",
+  "Tom",
+]; /* 배열 속 데이터 = 요소(Element), 각 순서대로 index 값을 가진다 (0부터 차례로 증가) 
+      ranking[0] --> "Jason" 
+      ranking[1] --> "Alice" 
+      ranking[2] --> "Chris" 
+      순서가 존재하는 데이터 창고 */
